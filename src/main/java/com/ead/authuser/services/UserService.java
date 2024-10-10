7db583b264e64bf7d99cc0e7926098abadb50f89
Bruno <bruno-ams@outlook.com>
2024-10-10 10:30:34 -0300

    advanced filters in api with Spectification using spring data

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -3,6 +3,7 @@ package com.ead.authuser.services;
 import com.ead.authuser.models.UserModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -22,5 +23,5 @@ public interface UserService {
 
     boolean existsByEmail(String email);
 
-    Page<UserModel> findAll(Pageable pageable);
+    Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable);
 }
